480272509f8e4802c751822fae742a45408103ee
Lahna13 <43355743+Lahna13@users.noreply.github.com>
2020-02-08 18:32:59 +0200

    Update Exel_JS.js

--- Exel_JS.js
+++ Exel_JS.js
@@ -1,1 +1,9 @@
 
+
+ function getInputValue(){
+    // Selecting the input element and get its value 
+    var inputValue = document.getElementById("Grinding Place").value;
+    
+    // Displaying the value
+    alert(inputValue);
+}
